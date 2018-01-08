071833cf62e5cab8813d7e3517208cb1fd6fb28f
ZAN DoYe <zandoye@gmail.com>
2018-01-08 22:24:12 +0800

    user/pswd auth

--- src/msg.ml
+++ src/msg.ml
@@ -4,7 +4,9 @@ let int16_to_net a= sprintf "%c%c"
   (a lsr 8 |> char_of_int)
   (a land 0xff |> char_of_int)
 
-let ver_s= '5'
+let ver_socks= '5'
+let ver_auth_userpswd= '1'
+
 type meth=
   | NoAuth
   | Gssapi
@@ -18,6 +20,14 @@ let meth_to_bin meth=
     | Gssapi-> 1
     | UserPswd-> 2
     | NoAcpt-> 0xff)
+
+let meth_of_bin bin=
+  match bin with
+  | 0-> NoAuth
+  | 1-> Gssapi
+  | 2-> UserPswd
+  | 0xff-> NoAcpt
+  | _-> sprintf "unkown method %d" bin |> failwith
 
 type cmd=
   | Cmd_connect
@@ -103,7 +113,7 @@ let rep_to_bin rep=
 
 let rep_request rep addr port=
   sprintf "%c%c\x00%s%s"
-    ver_s
+    ver_socks
     (rep_to_bin rep)
     (addr_to_bin addr)
     (int16_to_net port)
@@ -112,7 +122,7 @@ let rep_request_inet rep (inet:Unix.inet_addr) port=
   let inet:string= Obj.magic inet in
   let atyp= if String.length inet = 16 then 4 else 1 in
   sprintf "%c%c\x00%c%s%s"
-    ver_s
+    ver_socks
     (rep_to_bin rep)
     (char_of_int atyp)
     inet
@@ -125,20 +135,20 @@ let method_req methods=
     |> List.map ~f:meth_to_bin
     |> String.of_char_list
   in
-  sprintf "%c%c%s" ver_s (char_of_int n) methods
+  sprintf "%c%c%s" ver_socks (char_of_int n) methods
 
-let method_rep meth= sprintf "%c%c" ver_s (meth_to_bin meth)
+let method_rep meth= sprintf "%c%c" ver_socks (meth_to_bin meth)
 
 let request_req cmd addr port=
   sprintf "%c%c\x00%s%s"
-    ver_s
+    ver_socks
     (cmd_to_bin cmd)
     (addr_to_bin addr)
     (int16_to_net port)
 
 let request_rep rep addr port=
   sprintf "%c%c\x00%s%s"
-    ver_s
+    ver_socks
     (rep_to_bin rep)
     (addr_to_bin addr)
     (int16_to_net port)
@@ -149,4 +159,26 @@ let udp_datagram frag addr port data=
     (addr_to_bin addr)
     (int16_to_net port)
     data
+
+let auth_userpswd_req user pswd=
+  let ulen= min 255 (String.length user)
+  and plen= min 255 (String.length pswd) in
+  let user= String.sub user ~pos:0 ~len:ulen
+  and pswd= String.sub pswd ~pos:0 ~len:plen in
+  sprintf "%c%c%s%c%s"
+    ver_auth_userpswd
+    (char_of_int ulen)
+    user
+    (char_of_int plen)
+    pswd
+
+let auth_userpswd_rep status=
+  let status= char_of_int
+    (match status with
+    | true-> 0
+    | false-> 1)
+  in
+  sprintf "%c%c"
+    ver_auth_userpswd
+    status
 
